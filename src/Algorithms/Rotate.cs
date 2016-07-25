aa71ddf12c5eb7490ee1ed99b5c6572ce554bbb3
Gokhan Altinoren <gokhan@altinoren.com>
2016-07-25 17:00:44 +0300

    Implementing array rotation algorithm

---
+++ src/Algorithms/Rotate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace src.Algorithms
+{
+    public static partial class Algorithm
+    {
+        /// <summary>
+        /// Rotates (or shift while rotating the end to the begining) members of an array for given number of times.
+        /// Example: For 2, 1234 -> 2341 -> 3412
+        /// 
+        /// This algorithm has O(n) time complexity and O(1) space complexity.
+        /// One alternative is rotating the array one by one for number of iterations given,
+        /// which has O(n*iteration) time complexity and O(1) space complexity.
+        /// Another alternative is using a temporary array, with has O(n) and O(iteration)
+        /// time and space complexity respectively.
+        /// </summary>
+        public static void Rotate(this int[] array, int numberOfIterations)
+        {
+            if (numberOfIterations < 0)
+                throw new ArgumentOutOfRangeException();
+            if (numberOfIterations == 0 || numberOfIterations == array.Length)
+                return;
+            if (numberOfIterations > array.Length)
+                numberOfIterations = numberOfIterations%array.Length;
+
+            Array.Reverse(array, 0, numberOfIterations);
+            Array.Reverse(array, numberOfIterations, array.Length - numberOfIterations);
+            Array.Reverse(array);
+        }
+    }
+}
---
+++ src/Tests/Algorithms/RotateTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using src.Algorithms;
+using Xunit;
+
+namespace src.Tests.Algorithms
+{
+    public class RotateTests
+    {
+        [Fact]
+        public void Can_Rotate_Array()
+        {
+            var test = new[] {1, 3, 2, 4, 6, 5};
+            var expected = new[] {6, 5, 1, 3, 2, 4};
+            var times = 4;
+
+            test.Rotate(times);
+
+            Assert.True(Enumerable.SequenceEqual(test, expected));
+        }
+    }
+}
